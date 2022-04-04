46bf272508e3c608f013f0f0c4964f476ed0512c
Erol Sarp Konaklioglu <erolsarpk@gmail.com>
2022-04-04 11:26:23 +0300

    TaxiMeter added

---
+++ src/TaxiMeter.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class TaxiMeter {
+    public static void main(String[] args) {
+        double price, km, pricePerKm = 20;
+        int opening = 10, min = 20;
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please enter the total km travelled: ");
+        km = input.nextDouble();
+
+        price =  (km * pricePerKm) + opening;
+        price = (price > 20)? price:min;
+
+        System.out.println("Amount to pay: " + price);
+    }
+}
